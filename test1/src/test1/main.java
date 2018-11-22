b52e69f432421fecc065a1ed59f2135d7beaa9aa
ongszehua <ongsh-pa16@student.tarc.edu.my>
2018-11-22 23:11:46 +0800

    baba

---
+++ test1/src/test1/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test1;
+
+/**
+ * @author HUA
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
